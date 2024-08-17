62f550ed1d6838609d2f95c2f9f71fd18154e3db
Deldore <deldore.emil@yandex.ru>
2024-08-17 19:05:35 +0300

    header adaptive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import style from './HeaderStyle.module.css';
 const Header = () => {
+    const [menuState, setMenuState] = useState(false);
+
+    const [menuTop, setMenuTop] = useState(0);
+
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        setMenuTop(menuRef.current.clientHeight);
+    }, []);
+
     return (
         <div className={style.main}>
-            <div className={style.item}>
-                <ul className={style.list}>
+            <div className={style.item} ref={menuRef}>
+                <ul className={`${style.list} ${style.mobileHide}`}>
                     <li className={style.liActive}>
                         <a href="#">
                             Дневник
@@ -35,9 +45,32 @@ const Header = () => {
                             </span>
                         </div>
                     </li>
-                    <li>
+                    <li className={style.mobileHide}>
                         <a href="#">
                             Выйти
+                        </a>
+                    </li>
+                    <li>
+                        <button className={style.menuButton} onClick={() => {setMenuState(!menuState)}}>=</button>
+                    </li>
+                </ul>
+            </div>
+            <div className={style.menu} style={{left: (menuState) ? '0' : '-80vw', top: menuTop + 'px'}}>
+                <h1>Меню:</h1>
+                <ul className={`${style.menuList}`}>
+                    <li className={style.menuLiActive}>
+                        <a href="#">
+                            Дневник
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Расписание
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Сообщения
                         </a>
                     </li>
                 </ul>
